1851ab41f260ebdfb4ba214fc73feedb85a0272a
skchrko <skchrko@gmail.com>
2022-11-28 15:50:52 +0400

    dead code warning fix

--- scala-zio-example/src/main/scala/org/organization/Application.scala
+++ scala-zio-example/src/main/scala/org/organization/Application.scala
@@ -1,14 +1,14 @@
 package org.organization
 
+import org.organization.AppEnv.AppEnv
 import org.organization.config.HttpServerConfig
 import org.organization.http.swagger.SwaggerApiEndpoint
 import org.organization.utils.db.Migration
 import sttp.tapir.server.ziohttp.ZioHttpInterpreter
-import zhttp.http.HttpApp
-import zhttp.service.Server
-import zio._
 import zhttp.http._
-import AppEnv.{AppEnv, AppIO}
+import zhttp.service.{EventLoopGroup, Server, ServerChannelFactory}
+import zio._
+
 import java.time.Duration
 
 object Application extends App {
@@ -19,15 +19,13 @@ object Application extends App {
   private lazy val app: HttpApp[AppEnv, Throwable] =
     ZioHttpInterpreter().toHttp(SwaggerApiEndpoint.common) @@ composedMiddlewares
 
-  def main: AppIO[Unit] =
+  def main: ZIO[AppEnv, Throwable, ZIO[AppEnv with EventLoopGroup with ServerChannelFactory, Throwable, Nothing]] =
     for {
       _    <- Migration.migrate
       conf <- ZIO.service[HttpServerConfig]
-      // FIXME: Doesn't compile with the dead code compiler warning turned on
-      _ <- Server(app).withBinding(conf.host, conf.port).start
-    } yield ()
+    } yield Server(app).withBinding(conf.host, conf.port).start
 
-  def run(args: List[String]) = main
+  def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = main
     .provideLayer(AppEnv.buildLiveEnv)
     .exitCode
 }
