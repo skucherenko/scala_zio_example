a3319f5cf0434cd8d882c6eec48dd7f2d7fc7369
akolebakin <akolebakin@swiftinvention.com>
2022-11-28 15:09:41 +0400

    add schedule for trying to connect to db

--- scala-zio-example/src/main/scala/org/organization/AppEnv.scala
+++ scala-zio-example/src/main/scala/org/organization/AppEnv.scala
@@ -6,7 +6,9 @@ import javax.sql.DataSource
 import zhttp.service.EventLoopGroup
 import zhttp.service.server.ServerChannelFactory
 import zio._
+import zio.duration.durationInt
 import zio.clock.Clock
+import zio.console.putStrLn
 
 object AppEnv {
   type AppEnv = Has[HttpServerConfig]
@@ -17,7 +19,12 @@ object AppEnv {
 
   type AppIO[T] = ZIO[AppEnv, Throwable, T]
 
-  def buildLiveEnv: Layer[Serializable, AppEnv] =
-    HttpServerConfig.layer ++ Clock.live ++ DataSourceLayer.fromPrefix("mysql") ++ EventLoopGroup
-      .auto(0) ++ ServerChannelFactory.auto
+  lazy val availableDbSchedule = Schedule
+    .fixed(2000.milliseconds)
+    .tapOutput(o => putStrLn(s"Waiting for database to be available, retry count: $o").orDie)
+
+  def buildLiveEnv =
+    HttpServerConfig.layer ++ Clock.live ++
+      DataSourceLayer.fromPrefix("mysql").retry(availableDbSchedule) ++
+      EventLoopGroup.auto(0) ++ ServerChannelFactory.auto
 }
