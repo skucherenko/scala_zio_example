151a1a5535c602a64a325ef3ddec099aed046d4d
skchrko <skchrko@gmail.com>
2022-11-28 16:44:21 +0400

    dead code warning fix pt.2

--- scala-zio-example/src/main/scala/org/organization/Application.scala
+++ scala-zio-example/src/main/scala/org/organization/Application.scala
@@ -3,10 +3,9 @@ package org.organization
 import org.organization.AppEnv.AppEnv
 import org.organization.config.HttpServerConfig
 import org.organization.http.swagger.SwaggerApiEndpoint
-import org.organization.utils.db.Migration
 import sttp.tapir.server.ziohttp.ZioHttpInterpreter
 import zhttp.http._
-import zhttp.service.{EventLoopGroup, Server, ServerChannelFactory}
+import zhttp.service.Server
 import zio._
 
 import java.time.Duration
@@ -19,13 +18,15 @@ object Application extends App {
   private lazy val app: HttpApp[AppEnv, Throwable] =
     ZioHttpInterpreter().toHttp(SwaggerApiEndpoint.common) @@ composedMiddlewares
 
-  def main: ZIO[AppEnv, Throwable, ZIO[AppEnv with EventLoopGroup with ServerChannelFactory, Throwable, Nothing]] =
+  def main: ZIO[Has[HttpServerConfig], Nothing, Server[AppEnv, Throwable]] = {
     for {
-      _    <- Migration.migrate
       conf <- ZIO.service[HttpServerConfig]
-    } yield Server(app).withBinding(conf.host, conf.port).start
+      server = Server(app).withBinding(conf.host, conf.port)
+    } yield server
+  }
 
-  def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = main
-    .provideLayer(AppEnv.buildLiveEnv)
-    .exitCode
+  def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
+    val io = main.flatMap(_.start).provideLayer(AppEnv.buildLiveEnv)
+    io.exitCode
+  }
 }
